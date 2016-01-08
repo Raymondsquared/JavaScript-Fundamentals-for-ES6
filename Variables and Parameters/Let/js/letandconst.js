69fb00957da27a6c8ebfb5e84ead72281e45db44
Raymond.Raymond <raymond.raymond@carsales.com.au>
2016-01-08 15:23:26 +1100

    Let

---
+++ Variables and Parameters/Let/js/letandconst.js
@@ -0,0 +1,106 @@
+var chai = require('chai');
+
+var expect = chai.expect;
+
+describe("how let works", function(){
+	
+	it("will provide block scoping, unlike var (var)", function(){
+		
+		var doWork = function(flag){
+			if(flag){
+				var x = 3;
+			}
+
+			return x;
+		};
+
+		var result = doWork(true);
+		expect(result).to.equal(3);
+
+		/*
+		var result2 = doWork(false);
+		expect(result2).to.equal(3);
+		*/
+
+	});
+	
+	it("will provide block scoping, unlike var (let 1)", function(){		
+
+		var doWork = function(flag){
+			if(flag){
+				let x = 3;				
+			}
+
+			return x;
+		};
+
+		var result = doWork(true);		
+		
+		expect(result).to.equal(3);
+
+	});
+
+
+	it("will provide block scoping, unlike var (let 2)", function(){		
+
+		var doWork = function(flag){
+			if(flag){
+				let x = 3;
+				return x;
+			}
+		};
+
+		var result = doWork(true);		
+		
+		expect(result).to.equal(3);		
+
+	});
+
+	it("will provide block block scoping with for (var)", function(){		
+
+		var doWork = function(){
+			for(var i=0; i<10; i++){
+
+			}
+			return i;
+		};
+
+		var result = doWork();
+
+		expect(result).to.equal(10);
+
+	});
+
+
+	it("will provide block block scoping with for (let 1)", function(){		
+
+		var doWork = function(){
+			for(let i=0; i<10; i++){
+
+			}
+			return i;
+		};
+
+		var result = doWork();
+
+		expect(result).to.equal(10);
+
+	});
+
+	it("will provide block block scoping with for (let 2)", function(){		
+
+		var doWork = function(){
+			let i=0;
+			for(; i<10; i++){
+
+			}
+			return i;
+		};
+
+		var result = doWork();
+
+		expect(result).to.equal(10);
+
+	});
+
+});
